019c4fdb9fcd0abf58cb57f13e0c91e9cbd2b8d8
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2016-08-18 01:40:47 -0300

    Aula 30

---
+++ ASPNETDevMedia/SampleDataContext2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETDevMedia
+{
+    public partial class SampleDataContext
+    {
+        public void Update(Pessoa p)
+        {
+            this.UpdatePessoa(p);
+        }
+    }
+    public partial class Pessoa
+    {
+        public void Teste()
+        {
+            this.
+        }
+    }
+}
